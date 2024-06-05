314b8d8d05ca954642fbe94752c5b047c96202f3
kibaara-wangu <kibaarawangu@gmail.com>
2024-06-05 17:43:23 +0300

    Add login layout

--- app/src/main/java/com/akirachix/mentorconnect/LoginPage.kt
+++ app/src/main/java/com/akirachix/mentorconnect/LoginPage.kt
@@ -8,15 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val idLogin = findViewById<TextView>(R.id.idLogin)
-        idLogin.setOnClickListener {
-            val intent = Intent(this, LoginPage::class.java)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_page)
+       val idSignup = findViewById<TextView>(R.id.idSignup)
+        idSignup.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
